8b6cb645b5beb7bf0640caacbf2db6c02778199f
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-09-21 12:36:49 +0200

    ajout du fichier test BagTest

---
+++ projBag/src/projBag/BagTest.java
@@ -0,0 +1,84 @@
+package projBag;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BagTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIterator1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddE1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveAll() {
+		fail("Not yet implemented");
+	}
+
+}

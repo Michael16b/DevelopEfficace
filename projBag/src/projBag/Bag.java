1096d893fb243654c7db0cdcb0c8411a67a59c0b
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-09-25 17:20:48 +0200

    Implémentation de la javaDoc + de l'invariant

--- projBag/src/projBag/Bag.java
+++ projBag/src/projBag/Bag.java
@@ -6,43 +6,80 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+
+/***
+ * 
+ * @author Micha�l BESILY
+ *
+ */
+
 public class Bag<E> extends AbstractCollection<E> { 
 	private int size;
 	private Element sentinel;
+
 	
+	/**
+	 * It's a collection in which the data to be stored are inserted in any order.
+	 * 	There is no notion of primary key (in the database sense), so two identical data can coexist in the same collection.
+	 */
 	public Bag() {
 		this.sentinel = new Element(null,this.sentinel);
 		this.size = 0;
 	}
 	
-	
+	/**
+	 * @return This method gives the size of the Bag.
+	 */
 	public int size() {
 		return this.size;
 	}
 	
+	
+	/**
+	 * He inserts an element in random position on the bag.
+	 * @param Data - It's an object to add on the Bag.
+	 * @throws IllegalArgumentException if the data is null.
+	 * @throws NullPointerException if the next current or the actual current is null
+	 */
 	@Override
-	public boolean add(E data) {
+	public boolean add(E data) throws IllegalArgumentException,NullPointerException  {
+		if (data == null) throw new IllegalArgumentException("data is null");
+		
+		boolean ans = true;
 		if(this.size >= Integer.MAX_VALUE) {
-			return false;
+			ans = false;
 		}
 		
+		
+		if (ans) {
 		// java:S2140 - > Methods of "Random" that return floating point values should not be used in random integer generation
-		int index = (int) (Math.random() * (this.size + 1));
-		Element current = getElement(index);
-		
-		if (size == 0) {
-			current = new Element(data, this.sentinel);
-			this.sentinel.next = current;
-		} else {
-			Element newCurrentNext = current.next;
-			current.next = new Element(data, newCurrentNext);
+			int index = (int) (Math.random() * (this.size + 1));
+			Element current = getElement(index);
+			
+			if (size == 0) {
+				current = new Element(data, this.sentinel);
+				this.sentinel.next = current;
+			} else {
+				// Sonarlint: Null pointers should not be dereferenced (java:S2259)
+				try {
+					Element newCurrentNext = current.next;
+					current.next = new Element(data, newCurrentNext);
+				} catch(NullPointerException e) {
+			    	throw new NullPointerException("Next current or the actual current is null");
+				}
+			}
+			this.size++;
 		}
-		this.size++;
-		return true;
+		invariant();
+		return ans;
 		
 	}
 	
-	
+	/**
+	 * It's a collection in which the data to be stored are inserted in any order.
+	 * There is no notion of primary key (in the database sense), so two identical data can coexist in the same collection.
+	 * With this constructor we can put elements of an Collection<E>
+	 */
 	public Bag(Collection<E> c) {
 		this.sentinel = new Element(null,this.sentinel);
 		this.size = 0;
@@ -51,6 +88,7 @@ public class Bag<E> extends AbstractCollection<E> {
 			this.add(element);
 		}
 		
+		invariant();
 	}
 	
 	
@@ -64,14 +102,36 @@ public class Bag<E> extends AbstractCollection<E> {
 			}
 		if (e == null && e != this.sentinel) throw new NullPointerException();
 		
+		
+		invariant();
 		return e;
 	}
 
 	
-	
+	/**
+	 * Returns a new object of type java.util.Iterator (Java interface).
+	 */
 	public Iterator<E> iterator() {
 		return new Itr();
  }
+	
+	
+	
+	/***
+	 * @return He returns an boolean if the method finds an issue on each method.
+	 * @throws IllegalArgumentException. If the method find errors in the constructor he returns an IllegalArgumentException.
+	 */
+	private boolean invariant() throws IllegalArgumentException {
+		// Sonarlint : java:S3973
+		boolean res = true;
+		if (this.size < 0 || this.size > Integer.MAX_VALUE) {
+			res = !res;
+		}
+		if (!res) throw new IllegalArgumentException("Constructor Error");
+		else return res;
+	}
+	
+	
 	
 	private class Element {
 		private Element next; 
@@ -87,35 +147,76 @@ public class Bag<E> extends AbstractCollection<E> {
 		private Element current; 
 		private Element pastCurrent;
 		
-		
+		/** 
+		 * @param current is an Element
+		 * @param pastCurrent is a Element
+		 */
 		Itr() {
 			this.current = pastCurrent = Bag.this.sentinel;
 		}
-
+		
+		
+		/**
+		 * It searches if we can pass a next element
+		 * @return a boolean
+		 */
 		@Override
 		public boolean hasNext() {
 			return (this.current.next != Bag.this.sentinel);
 		}
 		
-		// Sonarlint : java:S2272 -> "Iterator.next()" methods should throw "NoSuchElementException"
+		
+		
+		
+		/**
+		 * He moves the current element in next	current.
+		 * @return If the current.next is null or it's the sentinel he returns false else he returns true
+		 * @throws NoSuchElementException if the method didn't find a new next current.
+		 */
 		@Override
 		public E next() throws NoSuchElementException {
+			// Sonarlint : java:S2272 -> "Iterator.next()" methods should throw "NoSuchElementException"
 			E data = null;
 			if (hasNext()) {
 				this.pastCurrent = this.current;
 				this.current = this.current.next;
 				data = this.current.data;
 			} else throw new NoSuchElementException();
+			invariant();
 			return data;
 		}
 		
+		
+		/**
+		 * This method removes the current element.
+		 * @return if the current has been remove he returns true.
+		 * @throws NoSuchElementException if the method didn't find an Element in the bag.
+		 */
 		@Override
-		public void remove() {
-			this.current = this.pastCurrent;
-			this.current.next = this.current.next.next;
-			Bag.this.size--;
+		public void remove() throws NoSuchElementException {
+			if (size > 0) {
+				this.current = this.pastCurrent;
+				this.current.next = this.current.next.next;
+				Bag.this.size--;
+			} else {
+				throw new NoSuchElementException("The iterator doesn't have an element to remove");
+			}
+			invariant();
 		}
 		
+		/***
+		 * @return He returns an boolean if the method finds an issue on each method.
+		 * @throws IllegalArgumentException. If the method find errors in the constructor he returns an IllegalArgumentException.
+		 */
+		private boolean invariant() throws IllegalArgumentException {
+			// Sonarlint : java:S3973
+			boolean res = true;
+			if ((this.current == null && this.current != Bag.this.sentinel) || (this.pastCurrent == null && this.pastCurrent != Bag.this.sentinel)) {
+				res = !res;
+			}
+			if (!res) throw new IllegalArgumentException("Constructor Error");
+			else return res;
+		}
 	}
 
 

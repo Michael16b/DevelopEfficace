ef3a8b3ff0505fbaf77628c361e06b3996ccda45
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-10-03 11:30:12 +0200

    autre

--- projArbreBin/src/datastruct/BinaryTreeTable.java
+++ projArbreBin/src/datastruct/BinaryTreeTable.java
@@ -1,35 +1,48 @@
-public class Node {
-    // Attributs
-    private Node lSon ; // fils gauche (null si pas de fils gauche)
-    private Node rSon ; // fils droit (null si pas de fils droit)
-    private Node father ; // père (null si le nœud est root)
-    private T theValue ; // donnée stockée
-    private E key ; // clé unique
+package datastruct;
+
+public  class BinaryTreeTable<K,T extends Comparable<Z>> implements Table<K,Z> {
+
+    private Node root = null;
+
+    class BinaryTreeTable<String t, int a> {
+        root = null;
+    }
 
 
-    public Node (T theValue, E key) {
-        this.lSon = null;
-        this.rSon = null;
-        this.father = null;
-        this.theValue = theValue;
-        this.key = key;
-    }
-    // Accesseurs
-    public String getLabel() {
-        return this.key;
-    }
-    public Node getLeft() {
-        return this.lSon;
-    }
-    public Node getRight() {
-        return this.rSon;
-    }
 
-    public Node clone() {
-        Node node = New Node(this.theValue, this.key);
-        node.father = this.father;
-        node.lSon = this.lSon;
-        node.rSon = this.rSon;
-    }
+    public class Node {
+        // Attributs
+        private Node lSon ; // fils gauche (null si pas de fils gauche)
+        private Node rSon ; // fils droit (null si pas de fils droit)
+        private Node father ; // père (null si le nœud est root)
+        private T theValue ; // donnée stockée
+        private E key ; // clé unique
 
+
+        public Node (T theValue, E key) {
+            this.lSon = null;
+            this.rSon = null;
+            this.father = null;
+            this.theValue = theValue;
+            this.key = key;
+        }
+        // Accesseurs
+        public String getLabel() {
+            return this.key;
+        }
+        public Node getLeft() {
+            return this.lSon;
+        }
+        public Node getRight() {
+            return this.rSon;
+        }
+
+        public Node clone() {
+            Node node = New Node(this.theValue, this.key);
+            node.father = this.father;
+            node.lSon = this.lSon;
+            node.rSon = this.rSon;
+        }
+
+    }
 }
